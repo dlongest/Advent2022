bd61c060f12595598cbe4362907121e68e9d2ec5
Daniel Longest <dlongest@ecomengine.com>
2022-11-28 14:54:10 -0500

    Ready for AoC Day 1

---
+++ Advent2022.Runner/Days/Day1.cs
@@ -0,0 +1,24 @@
+﻿using Advent2022.Runner;
+using Advent2022.Runner.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent2022.Runner.Days
+{
+    public class Day1 : IAdventProblem
+    {
+        public void A()
+        {
+            var numbers = FileSystem.MakeDataFilePath("day1")
+                                    .Read(line => Int32.Parse(line));  
+        }
+
+        public void B()
+        {
+           
+        }
+    }
+}
---
+++ Advent2022.Runner/RowColumnIndex.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent2022.Runner
+{
+    public class RowColumnIndex
+    {
+        public RowColumnIndex(int rowIndex, int columnIndex)
+        {
+            this.Row = rowIndex;
+            this.Column = columnIndex;
+        }
+
+        public static RowColumnIndex None = new RowColumnIndex(Int32.MinValue, Int32.MinValue);
+
+        public static RowColumnIndex New(int rowIndex, int columnIndex)
+        {
+            return new RowColumnIndex(rowIndex, columnIndex);
+        }
+
+        public int Row { get; private set; }
+
+        public int Column { get; private set; }
+
+        public override bool Equals(object obj)
+        {
+            var o = obj as RowColumnIndex;
+
+            if (o == null)
+            {
+                return false;
+            }
+
+            return this.Row == o.Row && this.Column == o.Column;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Row.GetHashCode() + 17 * this.Column.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"<{this.Row}, {this.Column}>";
+        }
+    }
+}
